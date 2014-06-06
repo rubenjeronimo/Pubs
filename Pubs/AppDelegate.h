eb6ed2be8f9c00c67c148fcd3e30d321b0d05f9a
Jose A. Herran <jherran@kedesfase.com>
2014-06-06 09:58:19 +0200

    Initial Commit

---
+++ Pubs/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pubs
+//
+//  Created by Jose A. Herran on 06/06/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
